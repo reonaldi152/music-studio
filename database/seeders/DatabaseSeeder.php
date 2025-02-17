faa1f7e178bf80fd944cfeeb3e8b02756e2cb379
reonaldi152 <reonaldi@team.superfk.co>
2025-02-17 21:00:16 +0700

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'roles' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('password'),
+            ]
+        ]);
+    }
+}
